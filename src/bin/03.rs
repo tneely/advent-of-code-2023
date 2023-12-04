48a9639880306371df1559707ef7b2d472168761
Taylor Neely <tayneely@amazon.com>
2023-12-04 14:49:08 -0800

    Day 3, part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -9,8 +9,23 @@ struct Part {
     y: i32,
 }
 
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct Gear {
+    symbol: char,
+    x: i32,
+    y: i32,
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut symbol_map: HashMap<i32, HashSet<i32>> = HashMap::new();
+    Some(solve(input).0)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(solve(input).1)
+}
+
+fn solve(input: &str) -> (u32, u32) {
+    let mut symbol_map: HashMap<i32, HashMap<i32, char>> = HashMap::new();
     let mut parts: Vec<Part> = Vec::new();
     input.lines().enumerate().for_each(|(y, line)| {
         let mut part_number_vec: Vec<u32> = Vec::new();
@@ -39,26 +54,29 @@ pub fn part_one(input: &str) -> Option<u32> {
 
                 if c != '.' {
                     symbol_map.entry(x as i32)
-                        .or_insert_with(|| HashSet::new())
-                        .insert(y as i32);
+                        .or_insert_with(|| HashMap::new())
+                        .insert(y as i32, c);
                 }
             }
         });
     });
 
     let mut sum: u32 = 0;
+    let mut gear_map: HashMap<Gear, Vec<u32>> = HashMap::new();
     parts.iter().for_each(|part| {
-        if contains_part(&symbol_map, &part) {
+        if let Some(gear) = contains_part(&symbol_map, &part) {
             sum += part.number;
-            // println!("{}", part.number);
+            gear_map.entry(gear)
+                .or_insert_with(|| Vec::new())
+                .push(part.number)
         }
     });
 
-    Some(sum)
-}
+    let gear_ratio: u32 = gear_map.iter()
+        .filter(|(gear, parts)| gear.symbol == '*' && parts.len() == 2)
+        .fold(0, |acc, (_, parts)| parts.iter().product::<u32>() + acc);
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+    (sum, gear_ratio)
 }
 
 fn vec_to_number(vec: &Vec<u32>) -> u32 {
@@ -67,35 +85,32 @@ fn vec_to_number(vec: &Vec<u32>) -> u32 {
     })
 }
 
-fn contains_part(symbol_map: &HashMap<i32, HashSet<i32>>, part: &Part) -> bool {
-    if part.number == 630 {
-        println!("hit")
-    }
+fn contains_part(symbol_map: &HashMap<i32, HashMap<i32, char>>, part: &Part) -> Option<Gear> {
     // Left
     if let Some(inner_map) = symbol_map.get(&(part.x1 - 1)) {
         // Upper
-        if inner_map.contains(&(part.y - 1)) {
-            return true
+        if let Some(c) = inner_map.get(&(part.y - 1)) {
+            return Some(Gear { symbol: *c, x: part.x1 - 1, y: part.y - 1 })
         }
         // Center
-        if inner_map.contains(&(part.y - 0)) {
-            return true
+        if let Some(c) = inner_map.get(&(part.y - 0)) {
+            return Some(Gear { symbol: *c, x: part.x1 - 1, y: part.y - 0 })
         }
         // Lower
-        if inner_map.contains(&(part.y + 1)) {
-            return true
+        if let Some(c) = inner_map.get(&(part.y + 1)) {
+            return Some(Gear { symbol: *c, x: part.x1 - 1, y: part.y + 1 })
         }
     }
 
     for x in part.x1..part.x2+1 {
         if let Some(inner_map) = symbol_map.get(&x) {
             // Upper
-            if inner_map.contains(&(part.y - 1)) {
-                return true
+            if let Some(c) = inner_map.get(&(part.y - 1)) {
+                return Some(Gear { symbol: *c, x: x, y: part.y - 1 })
             }
             // Lower
-            if inner_map.contains(&(part.y + 1)) {
-                return true
+            if let Some(c) = inner_map.get(&(part.y + 1)) {
+                return Some(Gear { symbol: *c, x: x, y: part.y + 1 })
             }
         }
     }
@@ -103,20 +118,20 @@ fn contains_part(symbol_map: &HashMap<i32, HashSet<i32>>, part: &Part) -> bool {
     // Right
     if let Some(inner_map) = symbol_map.get(&(part.x2 + 1)) {
         // Upper
-        if inner_map.contains(&(part.y - 1)) {
-            return true
+        if let Some(c) = inner_map.get(&(part.y - 1)) {
+            return Some(Gear { symbol: *c, x: part.x2 + 1, y: part.y - 1 })
         }
         // Center
-        if inner_map.contains(&(part.y - 0)) {
-            return true
+        if let Some(c) = inner_map.get(&(part.y - 0)) {
+            return Some(Gear { symbol: *c, x: part.x2 + 1, y: part.y - 0 })
         }
         // Lower
-        if inner_map.contains(&(part.y + 1)) {
-            return true
+        if let Some(c) = inner_map.get(&(part.y + 1)) {
+            return Some(Gear { symbol: *c, x: part.x2 + 1, y: part.y + 1 })
         }
     }
 
-    false
+    None
 }
 
 #[cfg(test)]
@@ -125,13 +140,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(4361));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
